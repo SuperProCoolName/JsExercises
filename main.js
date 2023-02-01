556848c9e76bd3ff82a5168c96141830aba2a9da
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-01 20:15:56 +0300

    007

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
 "use strict";
 
-let num = 123;
-let a = 1;
-let b = 2;
-let c = 3, d = 4;
-let e, f;
-e = 5;
-f = 6;
-alert('num = ' + num + '. My name is Emran. My surname is Minzatov. London is the capital of Great Britain. Дальше фигня просто: ' + b + c + d + e + f); // Тут комментарий короче
-let number = 10;
-alert('number = ' + number);
-number = 20;
-alert('number = ' + number);
+let a = 1 + 2 + 3;
+alert('My name is Emran. My surname is Minzatov. London is the capital of Great Britain. a = ' + a); // Тут комментарий короче
+
 /*
 А это тоже комментарий
 но на несколько строк
-
 короче
 */
