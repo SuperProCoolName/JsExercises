9d7f3bdfdc19e12def4a896806633b195deb8e29
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-01 15:55:17 +0300

    006

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 "use strict";
-alert('My name is Emran. My surname is Minzatov. London is the capital of Great Britain'); // Тут комментарий короче
 
+let num = 123;
+let a = 1;
+let b = 2;
+let c = 3, d = 4;
+let e, f;
+e = 5;
+f = 6;
+alert('num = ' + num + '. My name is Emran. My surname is Minzatov. London is the capital of Great Britain. Дальше фигня просто: ' + b + c + d + e + f); // Тут комментарий короче
+let number = 10;
+alert('number = ' + number);
+number = 20;
+alert('number = ' + number);
 /*
 А это тоже комментарий
 но на несколько строк
