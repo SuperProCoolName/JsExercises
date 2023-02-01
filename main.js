d41b875b071f797a987796c2dac3a53f57dcf8cd
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-01 20:37:17 +0300

    008

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 "use strict";
 
-let a = 1 + 2 + 3;
-alert('My name is Emran. My surname is Minzatov. London is the capital of Great Britain. a = ' + a); // Тут комментарий короче
-
+let a = 10;
+let b = 2;
+alert(`Zadanie 1. сумма = ${(a + b)} разность = ${(a - b)} произведение = ${(a * b)} частное = ${(a / b)}`); // Тут комментарий короче
+let c = 10;
+let d = 5;
+let result = c + d
+alert(`Zadanie 2. result = ${result}`);
+let e = 1;
+let f = 2;
+let g = 3;
+alert(`Zadanie 3. result = ${(e + f + g)}`);
+let h = 10;
+let i = 5;
+let j = h - i;
+let k = 7;
+let result4 = j + k;
+alert(`Zadanie 4. result = ` + result4);
 /*
 А это тоже комментарий
 но на несколько строк
