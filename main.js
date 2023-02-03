065e20a309785f29b272ccffa30eabf79b0353e5
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-03 17:37:26 +0300

    009

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
 "use strict";
 
-let a = 10;
-let b = 2;
-alert(`Zadanie 1. сумма = ${(a + b)} разность = ${(a - b)} произведение = ${(a * b)} частное = ${(a / b)}`); // Тут комментарий короче
-let c = 10;
-let d = 5;
-let result = c + d
-alert(`Zadanie 2. result = ${result}`);
-let e = 1;
-let f = 2;
-let g = 3;
-alert(`Zadanie 3. result = ${(e + f + g)}`);
-let h = 10;
-let i = 5;
-let j = h - i;
-let k = 7;
-let result4 = j + k;
-alert(`Zadanie 4. result = ` + result4);
+alert(`Placeholder potomu-chto 9 punkt na saite takoi`); // Тут комментарий короче
 /*
 А это тоже комментарий
 но на несколько строк
