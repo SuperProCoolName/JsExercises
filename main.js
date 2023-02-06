185ea0df4455a75054bb697e8ce6f85d4780d074
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-06 22:29:21 +0300

    012

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 "use strict";
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
-  
-
-let a = -100;
-let b = getRandomIntInclusive(-100, 100)
-alert(`a = ${a}`); // Тут комментарий короче
-alert(`negative b = ${b * -1}`);
+let a = 13;
+let b = 5
+alert(`a % b = ${a % b}`); // Тут комментарий короче
 /*
 А это тоже комментарий
 но на несколько строк
