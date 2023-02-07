b976e637d9b07af4975b3175acdcbc21480e47d2
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-07 15:47:46 +0300

    021

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 "use strict";
 
-const Pi = 3.14159265359, r = 15;
-alert(`Area with r = 15: ${Pi * r ** 2}`);
+let a = '5' + '2';
+console.log(a);
+let b = '5' + 2;
+console.log(b);
+let c = 5 + '2';
+console.log(c);
+let d = 5 + 2;
+console.log(d);
+let e = '5' * '2';
+console.log(e);
+let f = '5' - '2';
+console.log(f);
+let g = '5' / '2';
+console.log(g);
+let h = '5' % '2';
+console.log(h);
+let i = '5s' * '2';
+console.log(i);
+let j = '5s' + '2';
+console.log(j);
+let k = (-'5') + (-'2');
+console.log(k);
+let l = '5' * 1 + '2' * 1;
+console.log(l);
+let m = '5' * '1' + '2' * '1';
+console.log(m);
+let n = '' + 3 + 1;
+console.log(n);
 /*
 А это тоже комментарий
 но на несколько строк
