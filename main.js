586e137aebe523c1051bcce6e453abbd2bb4f8f9
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-07 16:01:35 +0300

    026

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
 "use strict";
 
-console.log(true + 3);
-console.log(true + true);
-console.log(true - true);
-console.log(true + false);
-console.log('1' + true);
-console.log( String(true) + 1 );
-console.log( String(true) + Number(true) );
+let test1 = Boolean(3);
+console.log(test1);
+let test2 = Boolean(0);
+console.log(test2);
+let test3 = Boolean(-1);
+console.log(test3);
+let test4 = Boolean(-0);
+console.log(test4);
+let test5 = Boolean(+0);
+console.log(test5);
+let test6 = Boolean('abc');
+console.log(test6);
+let test7 = Boolean('');
+console.log(test7);
+let test8 = Boolean('0');
+console.log(test8);
+let test9 = Boolean(true);
+console.log(test9);
+let test10 = Boolean(false);
+console.log(test10);
+let test11 = Boolean('true');
+console.log(test11);
+let test12 = Boolean('false');
+console.log(test12);
+let test13 = Boolean(null);
+console.log(test13);
+let test14 = Boolean('null');
+console.log(test14);
+let test15 = Boolean(undefined);
+console.log(test15);
+let test16 = Boolean('undefined');
+console.log(test16);
+let test17 = Boolean(NaN);
+console.log(test17);
+let test18 = Boolean('NaN');
+console.log(test18);
+let test19 = Boolean(3 * 'abc');
+console.log(test19);
+let test20 = Boolean(Infinity);
+console.log(test20);
+let test21 = Boolean(1 / 0);
+console.log(test21);
 /*
 А это тоже комментарий
 но на несколько строк
