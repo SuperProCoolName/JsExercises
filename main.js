86e05966310b117dc225d5322d93521956c9edaa
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-07 15:34:33 +0300

    019

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 "use strict";
 
-let a, b = null;
-alert(`${a}`);
-alert(`${b}`);
-let c = true, d = false;
-alert(`${c}`);
-alert(`${d}`);
-let str1 = "any text here", str2 = "something something anything";
-let str = str1 * str2;
-alert(`${str}`);
-alert(`${10 / 0}`);
-alert(`${-10 / 0}`);
+let a = "never";
+console.log(a);
+let b = "gonna", c = "give", d = "you", e = "up";
+console.log(b, c, d, e);
+let booleanVar = true, stringVar = "123", intVar = 123, undefinedVar = undefined, nullVar = null;
+console.log(booleanVar, stringVar, intVar, undefinedVar, nullVar);
+alert(errorBug);
 /*
 А это тоже комментарий
 но на несколько строк
