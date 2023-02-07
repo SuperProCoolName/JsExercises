f4b6d4f1a30759e64445e12812cd2fbd9f2a70d0
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-07 16:10:56 +0300

    028

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 "use strict";
 
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-
 let string = 'abcde';
-alert(`${string[0]}\n${string[2]}\n${string[4]}`);
-let gnirts = string[4] + string[3] + string[2] + string[1] + string[0];
-alert(gnirts);
-let num = randomIntFromInterval(0, 4);
-alert(`string[${num}] = ${string[num]}`);
+alert(`${string[string.length - 1]}`);
+alert(`${string[string.length - 2]}`);
+alert(`${string[string.length - 3]}`);
 /*
 А это тоже комментарий
 но на несколько строк
