65a26dc363193ded934c8b609e90b842a07a4cf0
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-07 15:51:18 +0300

    022

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
 "use strict";
 
-let a = '5' + '2';
-console.log(a);
-let b = '5' + 2;
-console.log(b);
-let c = 5 + '2';
-console.log(c);
-let d = 5 + 2;
-console.log(d);
-let e = '5' * '2';
-console.log(e);
-let f = '5' - '2';
-console.log(f);
-let g = '5' / '2';
-console.log(g);
-let h = '5' % '2';
-console.log(h);
-let i = '5s' * '2';
-console.log(i);
-let j = '5s' + '2';
-console.log(j);
-let k = (-'5') + (-'2');
-console.log(k);
-let l = '5' * 1 + '2' * 1;
-console.log(l);
-let m = '5' * '1' + '2' * '1';
-console.log(m);
-let n = '' + 3 + 1;
-console.log(n);
+let a = '10', b = '20';
+console.log(Number(a) + Number(b));
+console.log( Number('2') + Number('3') );
+console.log( 2 + Number('3') );
+console.log( '2' + Number('3') );
+let c = +'2';
+let d = +'3';
+console.log(c + d);
 /*
 А это тоже комментарий
 но на несколько строк
