83e3ca91ce76d78aa7ec7b2190f6f21a32210483
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-07 16:08:37 +0300

    027

--- main.js
+++ main.js
@@ -1,47 +1,15 @@
 "use strict";
 
-let test1 = Boolean(3);
-console.log(test1);
-let test2 = Boolean(0);
-console.log(test2);
-let test3 = Boolean(-1);
-console.log(test3);
-let test4 = Boolean(-0);
-console.log(test4);
-let test5 = Boolean(+0);
-console.log(test5);
-let test6 = Boolean('abc');
-console.log(test6);
-let test7 = Boolean('');
-console.log(test7);
-let test8 = Boolean('0');
-console.log(test8);
-let test9 = Boolean(true);
-console.log(test9);
-let test10 = Boolean(false);
-console.log(test10);
-let test11 = Boolean('true');
-console.log(test11);
-let test12 = Boolean('false');
-console.log(test12);
-let test13 = Boolean(null);
-console.log(test13);
-let test14 = Boolean('null');
-console.log(test14);
-let test15 = Boolean(undefined);
-console.log(test15);
-let test16 = Boolean('undefined');
-console.log(test16);
-let test17 = Boolean(NaN);
-console.log(test17);
-let test18 = Boolean('NaN');
-console.log(test18);
-let test19 = Boolean(3 * 'abc');
-console.log(test19);
-let test20 = Boolean(Infinity);
-console.log(test20);
-let test21 = Boolean(1 / 0);
-console.log(test21);
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+let string = 'abcde';
+alert(`${string[0]}\n${string[2]}\n${string[4]}`);
+let gnirts = string[4] + string[3] + string[2] + string[1] + string[0];
+alert(gnirts);
+let num = randomIntFromInterval(0, 4);
+alert(`string[${num}] = ${string[num]}`);
 /*
 А это тоже комментарий
 но на несколько строк
