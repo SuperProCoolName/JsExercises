6217d900bee5099a437ba6d18911dea60e2c6e99
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-12 16:27:15 +0300

    29

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 "use strict";
 
-let string = 'abcde';
-alert(`${string[string.length - 1]}`);
-alert(`${string[string.length - 2]}`);
-alert(`${string[string.length - 3]}`);
-/*
-А это тоже комментарий
-но на несколько строк
-короче
-*/
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+let str = '12345', num = 12345, sum_str = 0, sum_num = 0, comp_num = 1;
+for (let index = 0; index < str.length; index++) {
+    sum_str += Number(str[index]);
+};
+num = String(num);
+for (let index = 0; index < num.length; index++) {
+    sum_num += Number(num[index]);
+};
+for (let index = 0; index < num.length; index++) {
+    comp_num *= Number(num[index]);
+};
+
+alert(`Задание 1: ${sum_str}`);
+alert(`Задание 2: ${sum_num}`);
+alert(`Задание 3: ${comp_num}`);
+alert(`Задание 4: ${reverseString(num)}`);
