d36da82d7098ca7e77f96fe380452e499e620e5e
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-20 20:59:45 +0300

    73

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 "use strict";
 
-// пусто
+let test = Math.floor(Math.random() * 15);
+console.log(`test = ${test}`);
+if (test > 10) {
+  console.log(`test > 10`);
+} else if (test < 10) {
+  console.log(`test < 10`);
+} else if (test >= 10) {
+  console.log(`test >= 10`);
+} else if (test <= 10) {
+  console.log(`test <= 10`);
+}
