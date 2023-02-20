8f29bc988e7636e9a468e5467f4e45a32927a8ae
SuperProCoolName <minzatov.2004@mail.ru>
2023-02-20 21:12:06 +0300

    80

--- main.js
+++ main.js
@@ -2,27 +2,28 @@
 
 let test1 = "3";
 let test2 = "3";
-if (test1 == test2) {
+
+if (test1 != test2) {
   console.log("+++");
 } else {
   console.log("---");
 }
-if (test1 === test2) {
+if (test1 !== test2) {
   console.log("+++");
 } else {
   console.log("---");
 }
-if (test1 == test2) {
+if (test1 != test2) {
   console.log("+++");
 } else {
   console.log("---");
 }
-if (test1 === test2) {
+if (test1 !== test2) {
   console.log("+++");
 } else {
   console.log("---");
 }
-if (test1 === test2) {
+if (test1 !== test2) {
   console.log("+++");
 } else {
   console.log("---");
