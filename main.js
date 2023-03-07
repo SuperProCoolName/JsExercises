59bc4620e4b4d8d364d9b4b9d1ca4c3965090229
SuperProCoolName <minzatov.2004@mail.ru>
2023-03-07 19:44:22 +0300

    94

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 "use strict";
 
-let test = 10;
-if (test > 0) console.log("+++");
-else console.log("---");
-
-if (test > 0) console.log("+++");
+let min = Math.ceil(1);
+let max = Math.floor(32);
+let day = Math.floor(Math.random() * (max - min + 1) + min);
+console.log(day);
+if (day >= 1 && day < 10) document.write("1 декада");
+else if (day > 10 && day < 20) document.write("2 декада");
+else if (day > 20 && day <= 31) document.write("3 декада");
+else document.write("ошибка");
